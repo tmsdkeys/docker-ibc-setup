bb2cdc7d268ddfcddb0581f36738a25064a9c639
Thomas Dekeyser <thomas@interchain.io>
2022-11-23 13:59:14 +0100

    fix readme and delete tokentransfer folder

--- tokentransfer/checkers/build-images.sh
+++
@@ -1,2 +0,0 @@
-docker build -f Dockerfile . -t checkersa --build-arg configyml=./configa.yml --no-cache
-docker build -f Dockerfile . -t checkersb --build-arg configyml=./configb.yml --no-cache
--- tokentransfer/relayer_hermes/run-relayer.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-hermes keys add --chain checkersa --mnemonic-file "alice.json"
-hermes keys add --chain checkersb --mnemonic-file "bob.json"
-
-hermes create channel --a-chain checkersa --b-chain checkersb --a-port transfer --b-port transfer --new-client-connection
-hermes start
